d45ec1da48b5773144d4ec25da6f5b47c9e8f21d
Nicolas Rosales <nrosales.rs@gmail.com>
2025-10-24 00:38:05 +0000

    CH2-L4

--- functions/write_files_info.py
+++ functions/write_files_info.py
@@ -1,2 +1,18 @@
+import os
+
+
 def write_file(working_directory, file_path, content):
-    pass
+    try:
+
+        targer_path = os.path.abspath(os.path.join(working_directory, file_path)) #Si alguna de las rutas es absoluta, ignora working_directory.
+
+        if not targer_path.startswith(os.path.abspath(working_directory)):
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    
+        with open(targer_path, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        
+
+    except Exception as e:
+        return f'Error: {str(e)}'
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from functions.get_files_info import get_files_info
 from functions.get_files_info import get_file_content
+from functions.write_files_info import write_file
 
 if __name__ == "__main__":
     #print(get_files_info("calculator", "."))
@@ -7,7 +8,11 @@ if __name__ == "__main__":
     #print(get_files_info("calculator", "/bin"))
     #print(get_files_info("calculator", "../"))
 
-    print(get_file_content("calculator", "main.py"))
-    print(get_file_content("calculator", "pkg/calculator.py"))
-    print(get_file_content("calculator", "/bin/cat"))
-    print(get_file_content("calculator", "pkg/does_not_exist.py"))
+    #print(get_file_content("calculator", "main.py"))
+    #print(get_file_content("calculator", "pkg/calculator.py"))
+    #print(get_file_content("calculator", "/bin/cat"))
+    #print(get_file_content("calculator", "pkg/does_not_exist.py"))
+
+    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
